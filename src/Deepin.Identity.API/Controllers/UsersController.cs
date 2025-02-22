fd9c93bda1af5792b30329c65a9a382df6f7de30
Leo Yang <leoyang0105@gmail.com>
2025-02-22 11:57:35 +0800

    Update UsersController.cs

--- src/Deepin.Identity.API/Controllers/UsersController.cs
+++ src/Deepin.Identity.API/Controllers/UsersController.cs
@@ -1,15 +1,15 @@
 using Deepin.Domain;
 using Deepin.Identity.Application.Commands.Users;
-using Deepin.Identity.Application.Models.Users;
 using Deepin.Identity.Application.Queries;
 using MediatR;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Deepin.Identity.API.Controllers
 {
     [Route("api/v1/[controller]")]
     [ApiController]
+    [Authorize]
     public class UsersController(
         IMediator mediator,
         IUserQueries userQueries,
